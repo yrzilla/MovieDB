71844f890f47a57abc23dc244fde7518737f2e1f
YRZILLA <xbaracusx@mail.ru>
2022-05-07 15:08:56 +0300

    Создал обьект, добавил ответы на вопросы

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("Насколько вы его оцените?", ""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
+      d = prompt("Насколько вы его оцените?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
